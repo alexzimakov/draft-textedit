07c14c36bbb7a8b256d198dd16d631fcee216c51
Alexander Zimakov <alexzimakov@icloud.com>
2018-06-20 18:16:14 +0300

    define general variables

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 module.exports = {
   components: 'src/components/**/[A-Z]*.js',
+  require: [path.join(__dirname, 'src/DraftTextEdit.css')],
   webpackConfig: {
     module: {
       rules: [
