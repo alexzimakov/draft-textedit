5bc965d012a55ff916b757d4b31521e7ef8a7603
Alexander Zimakov <alexzimakov@icloud.com>
2018-07-01 15:19:23 +0300

    Tooltip component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as Popover } from './Popover';
+export { default as Tooltip } from './Tooltip';
 export { default as Button } from './Button';
 export { default as StyleButton } from './StyleButton';
-export { default as Popover } from './Popover';
