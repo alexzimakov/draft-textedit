d76f47c5e57f0b8cb61f6fd7bac20b9229ac5cf1
Alexander Zimakov <alexzimakov@icloud.com>
2018-07-02 20:51:57 +0300

    update Popover component

--- src/components/Popover/Popover.test.js
+++ src/components/Popover/Popover.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Popover from './Popover';
 
 function renderComponent(props = {}, mountFn = shallow) {
@@ -17,27 +17,69 @@ describe('<Popover /> component', () => {
     expect(popover.find('h3').text()).toBe('Popover test');
   });
 
-  it('should renders with custom style', () => {
-    const button = renderComponent({ style: { zIndex: 1 } });
-
-    expect(button.html().includes('z-index:1')).toBeTruthy();
-  });
-
-  it('should renders with custom class', () => {
-    const button = renderComponent({ className: 'popover-test' });
-
-    expect(button.hasClass('popover-test')).toBeTruthy();
-  });
-
   it('should renders with correct position', () => {
     const popover = renderComponent();
 
-    expect(popover.hasClass('DraftTextEditPopover_center')).toBeTruthy();
-
     popover.setProps({ position: 'left' });
-    expect(popover.hasClass('DraftTextEditPopover_left')).toBeTruthy();
+    expect(popover.hasClass('DraftTextEditPopover_position_left')).toBeTruthy();
 
     popover.setProps({ position: 'right' });
-    expect(popover.hasClass('DraftTextEditPopover_right')).toBeTruthy();
+    expect(
+      popover.hasClass('DraftTextEditPopover_position_right')
+    ).toBeTruthy();
+  });
+
+  it('should adds `mousedown` event handler in `componentDidMount` lifecycle method', () => {
+    jest.spyOn(document.body, 'addEventListener');
+
+    const popover = renderComponent();
+    const that = popover.instance();
+
+    expect(document.body.addEventListener).toHaveBeenCalledWith(
+      'mousedown',
+      that.handleBodyMouseDown
+    );
+  });
+
+  it('should removes `mousedown` event handler in `componentWillUnmount` lifecycle method', () => {
+    jest.spyOn(document.body, 'removeEventListener');
+
+    const popover = renderComponent();
+    const that = popover.instance();
+
+    popover.unmount();
+    expect(document.body.removeEventListener).toHaveBeenCalledWith(
+      'mousedown',
+      that.handleBodyMouseDown
+    );
+  });
+
+  it('should calls `onPressOutside` callback when click outside of popover', () => {
+    const onPressOutside = jest.fn();
+    const popover = renderComponent({ onPressOutside }, mount);
+    const event = new MouseEvent('mousedown');
+    const that = popover.instance();
+
+    that.handleBodyMouseDown(event);
+    expect(onPressOutside).toHaveBeenCalledWith(event);
+  });
+
+  it('should not calls `onPressOutside` callback when click inside of popover', () => {
+    const onPressOutside = jest.fn();
+    const popover = renderComponent({ onPressOutside }, mount);
+    const event = new MouseEvent('mousedown');
+    const that = popover.instance();
+
+    Object.defineProperty(event, 'target', { value: that.popoverRef.current });
+    that.handleBodyMouseDown(event);
+    expect(onPressOutside).not.toHaveBeenCalled();
+  });
+
+  it('should not throws an error if `onPressOutside` property is not given', () => {
+    const popover = renderComponent();
+
+    expect(() => {
+      popover.instance().props.onPressOutside();
+    }).not.toThrow();
   });
 });
