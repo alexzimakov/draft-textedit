3e57b28048709e07eefeae49cb41cc29f81b8e79
Alexander Zimakov <alexzimakov@icloud.com>
2018-07-09 19:34:23 +0300

    update StyleButton component

--- src/components/StyleButton/StyleButton.test.js
+++ src/components/StyleButton/StyleButton.test.js
@@ -13,21 +13,19 @@ function renderComponent(props = {}, mountFn = shallow) {
 }
 
 describe('<StyleButton /> component', () => {
-  it('should renders without errors', () => {
+  it('should renders with necessary components and children', () => {
     const styleButton = renderComponent();
 
-    expect(
-      styleButton.find('Button').hasClass('DraftTextEditStyleButton-Button')
-    ).toBeTruthy();
+    expect(styleButton.find('Tooltip')).toHaveLength(1);
+    expect(styleButton.find('Button')).toHaveLength(1);
+    expect(styleButton.contains(<FontAwesomeIcon icon={faBold} />)).toBeTruthy();
   });
 
   it('should adds a class with modifier for the `Button` element', () => {
     const styleButton = renderComponent({ active: true });
 
     expect(
-      styleButton
-        .find('Button')
-        .hasClass('DraftTextEditStyleButton-Button_active')
+      styleButton.find('Button').hasClass('DraftTextEditStyleButton-Button_active')
     ).toBeTruthy();
   });
 
@@ -53,53 +51,9 @@ describe('<StyleButton /> component', () => {
     }).not.toThrow();
   });
 
-  it('should renders with the `Tooltip` element if `title` property is given', () => {
-    const styleButton = renderComponent({ title: 'test' });
-
-    expect(styleButton.find('Tooltip')).toHaveLength(1);
-    expect(styleButton.find('Tooltip').props().children).toBe('test');
-    expect(styleButton.find('Button').props()['aria-describedby']).toBe(
-      styleButton.find('Tooltip').props().id
-    );
-  });
-
-  it('should shows and hides the tooltip', () => {
-    const styleButton = renderComponent({ title: 'test' });
-
-    styleButton
-      .find('Button')
-      .props()
-      .onMouseOver();
-    styleButton.update();
-    expect(
-      styleButton
-        .find('.DraftTextEditStyleButton-Tooltip')
-        .hasClass('DraftTextEditStyleButton-Tooltip_visible')
-    ).toBeTruthy();
-    styleButton
-      .find('Button')
-      .props()
-      .onMouseOut();
-    styleButton.update();
-    expect(
-      styleButton
-        .find('.DraftTextEditStyleButton-Tooltip')
-        .hasClass('DraftTextEditStyleButton-Tooltip_visible')
-    ).toBeFalsy();
-  });
-
   it('should not shows the tooltip if `disabled` property is true', () => {
     const styleButton = renderComponent({ title: 'test', disabled: true });
 
-    styleButton
-      .find('Button')
-      .props()
-      .onMouseOver();
-    styleButton.update();
-    expect(
-      styleButton
-        .find('.DraftTextEditStyleButton-Tooltip')
-        .hasClass('DraftTextEditStyleButton-Tooltip_visible')
-    ).toBeFalsy();
+    expect(styleButton.find('Tooltip').props().caption).toBeFalsy();
   });
 });
