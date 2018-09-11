5d71fc415df5a1d03774b2a3a41e8655b9123530
Alexander Zimakov <alexzimakov@icloud.com>
2018-09-11 11:28:10 +0300

    remove ControlsGroup component

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,4 @@ export { default as Select } from './Select';
 export { default as Button } from './Button';
 export { default as ToolbarButton } from './ToolbarButton';
 export { default as BlockTypeSelect } from './BlockTypeSelect';
-export { default as ControlsGroup } from './ControlsGroup';
 export { default as Editor } from './Editor';
